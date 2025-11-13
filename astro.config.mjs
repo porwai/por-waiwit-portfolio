5aceb387f8045bfc1fcb7f2f17fd10ba8a018b52
porwai <porwaiwit@gmail.com>
2025-11-12 20:50:32 -0500

    retry

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
   output: "static",
-  adapter: vercel(),
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
